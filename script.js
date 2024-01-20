e95c66dcecfea7dcb9581be018da14569664cd87
smriti <rajsimmi369@gmail.com>
2024-01-20 22:58:04 +0530

    it's done completely

--- script.js
+++ script.js
@@ -15,7 +15,16 @@ function addTask(){
     }
     inputBox.value="";
     saveData();
-};
+}
+
+listConatiner.addEventListener("click",function (e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checkedd");
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
 
 function saveData(){
     localStorage.setItem("data",listConatiner.innerHTML);
